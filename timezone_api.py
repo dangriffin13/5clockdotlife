ca5c97a6334ec336fee41564c7dea9f89834b38c
dangriffin13 <dangriffin13@gmail.com>
2019-05-01 00:05:24 -0400

    amdoren time zone check is working

--- timezone_api.py
+++ timezone_api.py
@@ -1,26 +1,28 @@
 import requests
 import urllib3
 import time
-import datetime
+from datetime import datetime
 import json
 
 
 #Using Amdoren API
 
-api_key = dRCi5M3LNtMbrPgqUwyangqG3DRbKF
+api_key = 'dRCi5M3LNtMbrPgqUwyangqG3DRbKF'
 
-base_url = https://www.amdoren.com/api/
+base_url = 'https://www.amdoren.com/api/'
 
-api_url = https://www.amdoren.com/api/timezone.php?api_key=dRCi5M3LNtMbrPgqUwyangqG3DRbKF
+timezone_url = 'https://www.amdoren.com/api/timezone.php?api_key=dRCi5M3LNtMbrPgqUwyangqG3DRbKF'
 
 
 def check_time_zone(location):
-    call_url = api_url + '&loc' + location
+    call_url = timezone_url + '&loc=' + location
 
-    response = requests.get(call_url)
-    print(response.status_code)
-    return response.json()
+    time_string = requests.get(call_url).json()['time']
 
+    
+    timestamp = datetime.strptime(time_string, "%Y-%m-%d %H:%M:%S")
+
+    return timestamp.hour
 
 def get_all_locations():
     call_url = base_url + 'locations.php?api_key=' + api_key
